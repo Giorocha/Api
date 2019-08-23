ff837e491fbcec95e1cab1d1aa2069e1cbb793d8
Giovanna Rocha Barros <43706052881@info.local>
2019-08-23 17:32:56 -0300

    up

---
+++ Senai.BookStore.WebApi/Senai.BookStore .ebApi/Controllers/AutoresController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.BookStore.ebApi.Domains;
+using Senai.BookStore.ebApi.Repositories;
+
+namespace Senai.BookStore_.ebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class AutoresController : ControllerBase
+    {
+        AutoresRepository autoresRepository = new AutoresRepository();
+
+        [HttpPost]
+        public IActionResult Cadastrar(AutoresDomain autor)
+        {
+            autoresRepository.Cadastrar(autor);
+            return Ok();
+        }
+    }
+}
---
+++ Senai.BookStore.WebApi/Senai.BookStore .ebApi/Repositories/AutoresRepository.cs
@@ -0,0 +1,32 @@
+﻿using Senai.BookStore.ebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.BookStore.ebApi.Repositories
+{
+    public class AutoresRepository
+    {
+        private string StringConexao = "Data Source=.\\SqlExpress; Initial Catalog=T_BookStore;User Id=sa;Pwd=132";
+
+        public void Cadastrar(AutoresDomain autoresDomain)
+        {
+            string Query = "insert into Autores (Nome, Email, Ativo, DataNascimento) values (@Nome, @Email, @Ativo, @DataNascimento)";
+
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                SqlCommand cmd = new SqlCommand(Query, con);
+                cmd.Parameters.AddWithValue("@Nome", autoresDomain.Nome);
+                cmd.Parameters.AddWithValue("@Email", autoresDomain.Email);
+                cmd.Parameters.AddWithValue("@Ativo", autoresDomain.Ativo);
+                cmd.Parameters.AddWithValue("@DataNascimento", autoresDomain.DataNascimento);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        
+    }
+}
