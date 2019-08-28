32245d8a180c8ea7b39ee05696adfbf68246ca2d
Giovanna Rocha Barros <43706052881@info.local>
2019-08-28 17:24:46 -0300

    up

---
+++ Senai.Ekpis.WebApi/Api/Senai.Ekpis.WebApi/Senai.Ekpis.WebApi/Controllers/FuncionariosController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Ekpis.WebApi.Domains;
+using Senai.Ekpis.WebApi.Repositories;
+
+namespace Senai.Ekpis.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class FuncionariosController : ControllerBase
+    {
+        FuncionarioRepository FuncionarioRepository = new FuncionarioRepository();
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(FuncionarioRepository.Listar());
+        }
+
+        [Authorize(Roles = "ADMINISTRADOR")]
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Funcionarios Funcionario = FuncionarioRepository.BuscarPorId(id);
+            if (Funcionario == null)
+                return NotFound();
+            return Ok(Funcionario);
+        }
+
+        [Authorize(Roles = "ADMINISTRADOR")]
+        [HttpPost]
+        public IActionResult Cadastrar(Funcionarios funcionario)
+        {
+            try
+            {
+                FuncionarioRepository.Cadastrar(funcionario);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { mensagem = "Erro ao cadastrar!" });
+            }
+        }
+
+        [Authorize(Roles = "ADMINISTRADOR")]
+        [HttpPut]
+        public IActionResult Atualizar(Funcionarios funcionario)
+        {
+            try
+            {
+                Funcionarios FuncionarioBuscado = FuncionarioRepository.BuscarPorId(funcionario.IdFuncionario);
+                if (FuncionarioBuscado == null)
+                    return NotFound();
+                FuncionarioRepository.Atualizar(funcionario);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { mensagem = "Erro ao atualizar!" });
+            }
+        }
+
+        [Authorize(Roles = "ADMINISTRADOR")]
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            FuncionarioRepository.Deletar(id);
+            return Ok();
+        }
+    }
+}
---
+++ Senai.Ekpis.WebApi/Api/Senai.Ekpis.WebApi/Senai.Ekpis.WebApi/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,60 @@
+﻿using Senai.Ekpis.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Ekpis.WebApi.Repositories
+{
+    public class FuncionarioRepository
+    {
+        public List<Funcionarios> Listar()
+        {
+            using (EkpisContext ctx = new EkpisContext())
+            {
+                return ctx.Funcionarios.ToList();
+            }
+        }
+
+        public Funcionarios BuscarPorId(int id)
+        {
+            using (EkpisContext ctx = new EkpisContext())
+            {
+                return ctx.Funcionarios.FirstOrDefault(x => x.IdFuncionario == id);
+            }
+        }
+
+        public void Cadastrar(Funcionarios Funcionario)
+        {
+            using (EkpisContext ctx = new EkpisContext())
+            {
+                ctx.Funcionarios.Add(Funcionario);
+                ctx.SaveChanges();
+
+            }
+        }
+
+        public void Atualizar(Funcionarios funcionario)
+
+        {
+            using (EkpisContext ctx = new EkpisContext())
+            {
+                Funcionarios FuncionarioBuscado = ctx.Funcionarios.FirstOrDefault(x => x.IdFuncionario == funcionario.IdFuncionario);
+                FuncionarioBuscado.Nome = funcionario.Nome;
+                ctx.Funcionarios.Update(FuncionarioBuscado);
+                ctx.SaveChanges();
+
+            }
+        }
+
+        public void Deletar(int id)
+        {
+            using (EkpisContext ctx = new EkpisContext())
+            {
+                Funcionarios Funcionario = ctx.Funcionarios.Find(id);
+                ctx.Funcionarios.Remove(Funcionario);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
